25eff3ca4af38a198e9a5c97977153a3bef21436
Greg Weber <greg@gregweber.info>
2018-02-06 08:06:47 -0800

    add React Disqus thread

--- gatsby-blog/src/templates/blog-post.js
+++ gatsby-blog/src/templates/blog-post.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Helmet from 'react-helmet'
 import Link from 'gatsby-link'
 import get from 'lodash/get'
+import ReactDisqusThread from 'react-disqus-thread'
 
 import Bio from '../components/Bio'
 import { rhythm, scale } from '../utils/typography'
@@ -65,6 +66,10 @@ class BlogPostTemplate extends React.Component {
             </li>
           )}
         </ul>
+        <ReactDisqusThread
+            shortname="shortname"
+            title={post.frontmatter.title}
+        />
       </div>
     )
   }
